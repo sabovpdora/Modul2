b2e6ea1bb9504d53c777c27cb7aaefe1a6b79295
sabovpdora <szabodora20@outlook.hu>
2020-05-26 12:31:38 +0200

    First Commit

---
+++ Controllers/SingletonController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Labka.Models;
+
+namespace Labka.Controllers
+{
+    public class SingletonController : ApiController
+    {
+        public HistoryDatabase1Entities db = new HistoryDatabase1Entities();
+        public Singleton Singleton = Singleton.GetInstance();
+        public IHttpActionResult Post([FromBody] Message_history message_history)
+        {
+            try
+            {
+                return Ok(Singleton.PostMessange(message_history));
+            }
+            catch (Exception error)
+            {
+                return InternalServerError(error);
+            }
+        }
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                return Ok(Singleton.GetHistory());
+            }
+            catch (Exception error)
+            {
+                return InternalServerError(error);
+            }
+        }
+        public void Delete()
+        {
+            Singleton.DeleteHistory();
+        }
+    }
+}
+
+    
+
---
+++ Models/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Labka.Models;
+
+namespace Labka.Models
+{
+    public class Singleton : ApiController
+    {
+        public HistoryDatabase1Entities db;
+        private Singleton()
+        {
+            this.db = new HistoryDatabase1Entities();
+        }
+        private static Singleton _instance;
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Singleton();
+            return _instance;
+        }
+        public List<Message_history> GetHistory()
+        {
+            return this.db.Message_history.ToList();
+        }
+        public List<Message_history> PostMessange(Message_history message)
+        {
+            this.db.Message_history.Add(message);
+            this.db.SaveChanges();
+            return this.db.Message_history.ToList();
+        }
+        public void DeleteHistory()
+        {
+            foreach (Message_history mh in db.Message_history)
+            {
+                db.Message_history.Remove(mh);
+            }
+            db.SaveChanges();
+        }
+    }
+}
